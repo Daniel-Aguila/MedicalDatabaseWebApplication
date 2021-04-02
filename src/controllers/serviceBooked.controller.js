e307f0612e2701fa9c7f8fae6fddff9cfda3297d
Daniel-Aguila <danielbaguila@gmail.com>
2021-04-01 19:52:25 -0500

    ServiceBooked

--- src/controllers/serviceBooked.controller.js
+++ src/controllers/serviceBooked.controller.js
@@ -2,7 +2,7 @@
 const ServiceBooked = require('../models/serviceBooked.model');
 
 // get all employee list
-exports.getserviceBookedList = (req, res)=> {
+exports.getServiceBookedList = (req, res)=> {
     //console.log('here all employees list');
     ServiceBooked.getAllServiceBookeds((err, serviceBookeds) =>{
         console.log('We are here');
@@ -14,9 +14,9 @@ exports.getserviceBookedList = (req, res)=> {
 }
 
 // get employee by ID
-exports.getServiceBookedByAppointmentID = (req, res)=>{
+exports.getServiceBookedByappointmentID = (req, res)=>{
     //console.log('get emp by id');
-    ServiceBooked.getServiceBookedByAppointmentID(req.params.id, (err, employee)=>{
+    ServiceBooked.getServiceBookedByappointmentID(req.params.id, (err, employee)=>{
         if(err)
         res.send(err);
         console.log('single employee data',employee);
@@ -24,43 +24,74 @@ exports.getServiceBookedByAppointmentID = (req, res)=>{
     })
 }
 
+exports.getServiceBookedByserviceID = (req, res)=>{
+    //console.log('get emp by id');
+    ServiceBooked.getServiceBookedByserviceID(req.params.id, (err, servicebooked)=>{
+        if(err)
+        res.send(err);
+        console.log('single employee data',servicebooked);
+        res.send(servicebooked);
+    })
+}
+
+exports.getServiceBookeddurationID = (req, res)=>{
+    //console.log('get emp by id');
+    ServiceBooked.getServiceBookedBydurationID(req.params.id, (err, servicebooked)=>{
+        if(err)
+        res.send(err);
+        console.log('single employee data',servicebooked);
+        res.send(servicebooked);
+    })
+}
+
+exports.getServiceBookedpriceID = (req, res)=>{
+    //console.log('get emp by id');
+    ServiceBooked.getServiceBookedBypriceID(req.params.id, (err, servicebooked)=>{
+        if(err)
+        res.send(err);
+        console.log('single employee data',servicebooked);
+        res.send(servicebooked);
+    })
+}
+
 // create new employee
-exports.createNewEmployee = (req, res) =>{
-    const employeeReqData = new ServiceBooked(req.body);
-    console.log('employeeReqData', employeeReqData);
+exports.createNewServiceBooked = (req, res) =>{
+    const serviceBookedeReqData = new ServiceBooked(req.body);
+    console.log('serviceBookedeReqData', serviceBookedeReqData);
     // check null
     if(req.body.constructor === Object && Object.keys(req.body).length === 0){
         res.send(400).send({success: false, message: 'Please fill all fields'});
     }else{
-        ServiceBooked.createEmployee(employeeReqData, (err, employee)=>{
+        ServiceBooked.createServiceBooked(serviceBookedeReqData, (err, servicebooked)=>{
             if(err)
             res.send(err);
-            res.json({status: true, message: 'Employee Created Successfully', data: employee.insertId})
+            res.json({status: true, message: 'Service Booked Created Successfully', data: servicebooked.insertId})
         })
     }
 }
 
 // update employee
-exports.updateEmployee = (req, res)=>{
-    const employeeReqData = new ServiceBooked(req.body);
-    console.log('employeeReqData update', employeeReqData);
+exports.updateServiceBooked = (req, res)=>{
+    const serviceBookedeReqData = new ServiceBooked(req.body);
+    console.log('serviceBookedeReqData update', serviceBookedeReqData);
     // check null
     if(req.body.constructor === Object && Object.keys(req.body).length === 0){
         res.send(400).send({success: false, message: 'Please fill all fields'});
     }else{
-        ServiceBooked.updateEmployee(req.params.id, employeeReqData, (err, employee)=>{
+        ServiceBooked.updateServiceBooked(req.params.id, serviceBookedeReqData, (err, servicebooked)=>{
             if(err)
             res.send(err);
-            res.json({status: true, message: 'Employee updated Successfully'})
+            res.json({status: true, message: 'ServiceBooked updated Successfully'})
         })
     }
 }
 
+//employee = servicebooked
 // delete employee
-exports.deleteEmployee = (req, res)=>{
-    ServiceBooked.deleteEmployee(req.params.id, (err, employee)=>{
+exports.deleteServiceBooked = (req, res)=>{
+    ServiceBooked.deleteServiceBooked(req.params.id, (err, servicebooked)=>{
         if(err)
         res.send(err);
-        res.json({success:true, message: 'Employee deleted successully!'});
+        res.json({success:true, message: 'ServiceBooked deleted successully!'});
     })
 }
--- src/routes/serviceBooked.route.js
+++ src/routes/serviceBooked.route.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-const employeeController = require('../controllers/employee.controller');
+const employeeController = require('../controllers/serviceBooked.controller');
 
 // get all employees
-router.get('/', employeeController.getEmployeeList);
+router.get('/', employeeController.getServiceBookedList);
 
 // get employee by ID
-router.get('/:id',employeeController.getEmployeeByID);
+router.get('/:id',employeeController.getServiceBookedByappointmentID);
+
+router.get('/:id',employeeController.getServiceBookedByserviceID);
+
+router.get('/:id',employeeController.getServiceBookeddurationID);
+
+router.get('/:id',employeeController.getServiceBookedpriceID);
 
 // create new employee
-router.post('/', employeeController.createNewEmployee);
+router.post('/', employeeController.createNewServiceBooked);
 
 // update employee
-router.put('/:id', employeeController.updateEmployee);
+router.put('/:id', employeeController.updateServiceBooked );
 
 // delete employee
-router.delete('/:id',employeeController.deleteEmployee);
+router.delete('/:id',employeeController.eleteServiceBooked);
 
 module.exports = router;
