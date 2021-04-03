126a0cc320da1739dd6954c8a462f4c992d19fe9
MJakeShapiro <49694168+MJakeShapiro@users.noreply.github.com>
2021-04-03 12:58:07 -0500

    staffDelete, staffUpdate, layout update

--- src/routes/staff.route.js
+++ src/routes/staff.route.js
@@ -3,29 +3,50 @@ const router = express.Router();
 
 const staffController = require('../controllers/staff.controller');
 
+// render staff front page
 router.get('/', (reg,res)=>{
     res.render('./actions/staffActions');
 });
 
-// get all staffs
-router.get('/', (staffController.getStaffList));
-
-// create new Staff
+// render create new staff page
 router.get('/new', (req,res)=>{
-    res.render('./new/newStaff');
+    res.render('./new/staffNew');
+});
+
+// render update staff page
+router.get('/update', (req,res)=>{
+    res.render('./update/staffUpdate')
 })
 
+//render view staff by ID page
+router.get('/view', (req,res)=>{
+    res.render('./view/staffViewID')
+})
+
+// render view all staff page
+router.get('/viewAll', (req,res)=>{
+    res.render('/viewAll/staffViewAll')
+})
+
+// render delete staff page
+router.get('/delete', (req,res)=>{
+    res.render('./delete/staffDelete');
+});
+
+
+// create new staff
 router.post('/new', staffController.createNewStaff);
 
-// get Staff by ID
-router.get('/:id',staffController.getStaffByID);
+// view all staffs
+router.get('/view', (staffController.getStaffList));
 
-
+// view staff by ID
+router.get('/viewAll/:id',staffController.getStaffByID);
 
 // update staff
-router.put('/:id', staffController.updateStaff);
+router.put('/update/:id', staffController.updateStaff);
 
 // delete staff
-router.delete('/:id',staffController.deleteStaff);
+router.post('/delete/:id',staffController.deleteStaff);
 
 module.exports = router;
